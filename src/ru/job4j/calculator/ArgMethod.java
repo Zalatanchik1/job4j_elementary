8a07b0934c29f5a510f032c71621f2ae6b9ceab4
Braveheart <ai.golubevsanya@gmail.com>
2021-04-16 18:11:34 +0400

    4.1. Аргументы [#518997]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -1,14 +1,14 @@
 package ru.job4j.calculator;
 
 public class ArgMethod {
-    public static void hello(String name) {
+    public static void hello(String name, int age) {
         System.out.println("Hello," + name);
+        System.out.println(age);
     }
 
     public static void main(String[] args) {
         String name = "Alex Golubev";
         int age = 24;
-
-        ArgMethod.hello(name);
+        ArgMethod.hello(name,age);
     }
 }

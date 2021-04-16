c94099f08fe6442466937df3c9dd0baf8c2cf833
Braveheart <ai.golubevsanya@gmail.com>
2021-04-16 14:18:19 +0400

    0. Что такое переменная. [#519001]

---
+++ src/ru/job4j/Variables.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 18;
+        float lenght = 78.0332F;
+        boolean baby = true;
+        System.out.println(age);
+        System.out.println(lenght);
+        System.out.println(baby);
+    }
+}

c5f461935cf1d563551e718a964158e569b8b08c
Braveheart <ai.golubevsanya@gmail.com>
2021-04-19 13:13:30 +0400

    5. Конвертер валюты [#518985]

---
+++ src/ru/job4j/calculator/MathFunc1.java
@@ -0,0 +1,22 @@
+package ru.job4j.calculator;
+
+public class MathFunc1 {
+
+    public static int func1(int x) {
+        int y = x * x + 1;
+        return y;
+    }
+
+    public static int func2(int x) {
+        int y = 1 / x;
+        return y;
+    }
+
+    public static void main(String[] args) {
+        int result1 = MathFunc1.func1(100);
+        int result2 = MathFunc1.func2(5);
+        int result = result1 + result2;
+        System.out.println(result);
+    }
+}
+// Задача: необходимо вызвать метод func1 с аргументом х = 100, а результат записать в переменную result.

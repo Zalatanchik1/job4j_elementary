7fbbab84bc88ded709f2aba389a94d60c0211d64
Braveheart <ai.golubevsanya@gmail.com>
2021-04-20 13:24:05 +0400

    9. Площадь треугольника. [#518992]

---
+++ src/ru/job4j/comdition/Greeting.java
@@ -0,0 +1,9 @@
+package ru.job4j.comdition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!" + "But I am a newbie.";
+        int year = 2021;
+        System.out.println(idea + year);
+    }
+}

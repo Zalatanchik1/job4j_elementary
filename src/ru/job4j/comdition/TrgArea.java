bfa8ff145567fe7b4e75cf466fc5d0183d333b4f
Braveheart <ai.golubevsanya@gmail.com>
2021-04-21 15:20:14 +0400

    9. Площадь треугольника. [#518992]

--- src/ru/job4j/comdition/TrgArea.java
+++ src/ru/job4j/comdition/TrgArea.java
@@ -1,19 +1,14 @@
 package ru.job4j.comdition;
 
 public class TrgArea {
-    public static double area(double a, double b, double c, double p) {
-        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
-    }
-
-    public static double halfMeter(double a, double b, double c) {
-        return a + b + c;
-
+    public static double area(double a, double b, double c) {
+        double halfMeter = (a + b + c) / 2;
+        double square = Math.sqrt(halfMeter * (halfMeter - a) * (halfMeter - b) * (halfMeter - c));
+        return square;
     }
 
     public static void main(String[] args) {
-        double rsl = TrgArea.halfMeter(2, 2, 2);
-        double rsl1 = TrgArea.area(2, 2, 2, rsl);
-        System.out.println("Полупермитр треугольника равен: p = " + rsl);
-        System.out.println("Площадь треугольника равна: S = " + rsl1);
+        double rsl = TrgArea.area(2, 2, 2);
+        System.out.println(rsl);
     }
 }

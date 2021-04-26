c841b20d4897319a3e3c535072397cf4e1cfa47f
Braveheart <ai.golubevsanya@gmail.com>
2021-04-26 17:52:04 +0400

    4.1.2. Глупый бот [#518929]

--- src/ru/job4j/comdition/DummyBot.java
+++ src/ru/job4j/comdition/DummyBot.java
@@ -5,15 +5,11 @@ import java.sql.SQLOutput;
 public class DummyBot {
 
     public static String answer(String question) {
-        System.out.println(question);
-        String rsl;
-        if (question.equals("Привет, Бот")) {
-            rsl = "Привет, умник";
-        }
-        else if (question.equals("Пока.")) {
-            rsl = "До скорой встречи!";
-        } else {
-            rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if (question.equals("Привет, Бот.")) {
+            rsl = "Привет, умник.";
+        } else if (question.equals("Пока.")) {
+            rsl = "До скорой встречи.";
         }
         return rsl;
     }

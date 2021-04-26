db45f9f3374d7aae2dfb0f15707a0d0d4d9d1ba8
Zalatanchik1 <ai.golubevsanya@gmail.com>
2021-04-26 21:46:04 +0400

    4.2. Тернарное сравнение [#518946]

---
+++ src/ru/job4j/comdition/Max.java
@@ -0,0 +1,27 @@
+package ru.job4j.comdition;
+
+public class Max {
+    public static int max(int left, int right) {
+        boolean condition = false;
+        int result = condition ? left : right;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int max = Max.max(1, 2);
+        System.out.println(max);
+    }
+}
+/*
+public class Film {
+    public static String permission(int age) {
+        String label = age >= 18 ? "Please, enjoy it." : "You can't watch it.";
+        return label;
+    }
+
+    public static void main(String[] args) {
+        String msg = Film.permission(33);
+        System.out.println(msg);
+    }
+}
+ */
---
+++ test/ru/job4j/comdition/MaxTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.comdition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaxTest {
+
+    @Test
+    public void whenMax1To2Then2() {
+        int left = 1;
+        int right = 2;
+        int result = Max.max(left, right);
+        int expected = 2;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenFirstMax() {
+        int left = 3;
+        int right = 4;
+        int result = Max.max(right, left);
+        int expected = 3;
+        Assert.assertEquals(result,expected);
+    }
+
+    @Test
+    public void whenSecondMax() {
+        int left = 3;
+        int right = 4;
+        int result = Max.max(left, right);
+        int expected = 4;
+        Assert.assertEquals(result,expected);
+    }
+
+    @Test
+    public  void whenFirstAndSecond() {
+        int left = 4;
+        int right = 4;
+        int result = Max.max(left, right);
+        int expected = 4;
+        Assert.assertEquals(result,expected);
+    }
+}

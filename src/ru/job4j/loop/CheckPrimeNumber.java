c9e907a3c5a1a40106f7ce56564ed13ea3d23227
Zalatanchik1 <ai.golubevsanya@gmail.com>
2021-06-21 21:33:22 +0400

    6.0.5. Массивы в тестах. [#226789]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,7 +2,13 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = number > 1;
+        boolean prime = true;
+
+        if (number == 1) {
+            prime = false;
+            return prime;
+        }
+
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 prime = false;
